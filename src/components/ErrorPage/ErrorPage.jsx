80065c7dd03c234c99c4367e902fb0f0f7349e91
Mmsshakil <softhandshakil@gmail.com>
2023-09-25 00:37:26 +0600

    navbar addded and error page added by react route in main.jsx file

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Oopppss !!!!!!!!</h2>
+            <Link to='/'>Go Back to Home</Link>
+            
+        </div>
+    );
+};
+
+export default ErrorPage;
